4f8f838a2aff0e5f4a543e7f16c304a7817be9e4
Gastoon-rep <87291111+Gastoon-rep@users.noreply.github.com>
2021-09-10 00:40:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const { readdirSync } = require("fs");
+const client = new Discord.Client({
+    intents: 32767,
+});
+const token = process.env['token']
+
+client.on('ready', () => {
+   console.log(`¡Estoy listo!`);
+});
+
+client.comandos = new Discord.Collection()
+// Comandos
+for (const subFolder of readdirSync(`${__dirname}/comandos/`)) {
+    for (const fileName of readdirSync(`${__dirname}/comandos/${subFolder}/`)) {
+        let file = require(`${__dirname}/comandos/${subFolder}/${fileName}`);
+       client.comandos.set(file.name, file);
+    }
+}
+
+// Eventos
+for (const fileName of readdirSync(`${__dirname}/eventos/`)) {
+    let file = require(`${__dirname}/eventos/${fileName}`);
+    let eventEmiter = file.emiter;
+
+    client[eventEmiter](file.name, file.run.bind(null, client));
+}
+
+client.login(token);
